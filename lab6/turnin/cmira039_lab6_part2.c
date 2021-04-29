4091ba907bc8bf2c3f386db600c8592c4f8ac436
Kotori <8682236+kotooriiii@users.noreply.github.com>
2021-04-28 17:57:56 -0700

    Update cmira039_lab6_part2.c

--- lab6/turnin/cmira039_lab6_part2.c
+++ lab6/turnin/cmira039_lab6_part2.c
@@ -7,7 +7,7 @@
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  *
- *	Demo Link: 
+ *	Demo Link: https://youtu.be/R0yl864lv1s
  */
  
 #include <avr/io.h>
